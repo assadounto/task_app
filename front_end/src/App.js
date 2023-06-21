595de2d78bcee3b59127abe6f755774595f53659
rich <adukyerer@gmail.com>
2023-06-21 21:28:45 +0000

    Add mark as completed feature

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Typography, Input, Button, List, message,Checkbox } from 'antd';
+import { DeleteOutlined } from '@ant-design/icons';
+import 'antd/dist/reset.css';
+
 
 const API_BASE_URL = 'http://localhost:3000/tasks';
-
 function App() {
   const [tasks, setTasks] = useState([]);
   const [title, setTitle] = useState('');
@@ -21,12 +24,12 @@ function App() {
     }
   };
 
-  const createTask = async (e) => {
-     e.preventDefault()
+  const createTask = async () => {
     try {
       const response = await axios.post(API_BASE_URL, {
         title,
         description,
+        completed: false, // Set completed to false initially
       });
       setTasks([...tasks, response.data]);
       setTitle('');
@@ -45,34 +48,71 @@ function App() {
     }
   };
 
+  const toggleCompletion = async (taskId, completed) => {
+    try {
+      await axios.put(`${API_BASE_URL}/${taskId}`, {
+        completed: !completed, // Toggle the completed status
+      });
+      setTasks((prevTasks) =>
+        prevTasks.map((task) =>
+          task.id === taskId ? { ...task, completed: !completed } : task
+        )
+      );
+    } catch (error) {
+      console.error('Error updating task:', error);
+    }
+  };
+
   return (
-    <div>
-      <h1>Task Manager</h1>
-      <form onSubmit={createTask}>
-        <input
-          type="text"
-          placeholder="Title"
-          value={title}
-          onChange={(e) => 
-             setTitle(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        <button type="submit">Create Task</button>
-      </form>
-      <ul>
-        {tasks.map((task) => (
-          <li key={task.id}>
-            <h2>{task.title}</h2>
-            <p>{task.description}</p>
-            <button onClick={() => deleteTask(task.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+    <div style={{ maxWidth: '600px', margin: '0 auto', padding: '20px' }}>
+      <Typography.Title level={2}>Task Manager</Typography.Title>
+      <Input
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        style={{ marginBottom: '10px' }}
+      />
+      <Input.TextArea
+        placeholder="Description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        rows={4}
+        style={{ marginBottom: '10px' }}
+      />
+      <Button
+        type="primary"
+        onClick={createTask}
+        disabled={!title.trim()}
+        style={{ marginBottom: '20px' }}
+      >
+        Create Task
+      </Button>
+      <List
+        dataSource={tasks}
+        renderItem={(task) => (
+          <List.Item
+            key={task.id}
+            actions={[
+              <Checkbox
+                checked={task.completed}
+                onChange={() => toggleCompletion(task.id, task.completed)}
+              >
+                Completed
+              </Checkbox>,
+              <Button
+                type="text"
+                icon={<DeleteOutlined />}
+                onClick={() => deleteTask(task.id)}
+              />,
+            ]}
+          >
+            <List.Item.Meta
+              title={task.title}
+              description={task.description}
+            />
+          </List.Item>
+        )}
+      />
     </div>
   );
 }
